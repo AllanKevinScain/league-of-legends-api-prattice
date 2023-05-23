e81142b9d60a4b0d68bc0d81a86371278a46a460
allankevinscain <meuemail44allan@gmail.com>
2023-05-23 16:52:19 -0300

    fix: https

--- src/utils/get-champions.js
+++ src/utils/get-champions.js
@@ -1,5 +1,5 @@
 const getChampions = async (lenguage = 'pt_BR') => {
-  const fetcher = fetch(`http://ddragon.leagueoflegends.com/cdn/13.10.1/data/${lenguage}/champion.json`);
+  const fetcher = fetch(`https://ddragon.leagueoflegends.com/cdn/13.10.1/data/${lenguage}/champion.json`);
   
   const [champions] = await Promise.all([fetcher]);
 
